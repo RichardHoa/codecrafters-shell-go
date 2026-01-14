a5586a8a673de75f6ff84e93c13590ab937ab258
Linux <richardhoa.work@gmail.com>
2026-01-14 22:50:50 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -8,15 +8,24 @@ import (
 
 func main() {
 	for {
-
 		fmt.Print("$ ")
+
 		scanner := bufio.NewScanner(os.Stdin)
+
 		scanner.Scan()
+
 		err := scanner.Err()
 		if err != nil {
 			fmt.Printf("Fatal error: %v\n", err)
 		}
-		fmt.Printf("%s: command not found\n", scanner.Text())
+
+		input := scanner.Text()
+
+		if input == "exit" {
+			break
+		}
+
+		fmt.Printf("%s: command not found\n", input)
 
 	}
 
